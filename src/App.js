84e7b0ca637648847629f11645335c37f7118171
ShoetaBadani <143995573+ShoetaBadani@users.noreply.github.com>
2023-11-06 13:12:34 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { HeroSection } from './components/HeroSection';
+import { Projects } from './components/Projects';
+import { BuyerReview, SellerReview } from './components/Reviews';
+import Contact from './components/Contact';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+      <HeroSection/>
+      <Projects/>
+      <BuyerReview/>
+      <SellerReview/>
+      <Contact/>
+    
+      
+    </div>
+  );
+}
+
+export default App;
